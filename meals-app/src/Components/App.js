b03cbaf7f96e60971c87b2cc495fa182460d1ee4
hx08371 <brammannjeffreyt@johndeere.com>
2020-08-21 15:40:46 -0500

    App.js issue

--- meals-app/src/Components/App.js
+++ meals-app/src/Components/App.js
@@ -1,29 +1,30 @@
 import React, { Component } from "react";
-import "./App.css";
-import Meals from "./Meals.js";
+//import "../App.css";
+import Meals from "./Component/Meals";
 import axios from "axios";
+import Meals from "./Components/Meals";
 
 const mealsURL = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			mealsList: [],
-		};
-	}
-	//   getMeals = async (event) => {
-	componentDidMount = async () => {
-		let response = await axios.get(mealsURL);
+  constructor() {
+    super();
+    this.state = {
+      mealsList: [],
+    };
+  }
+  //   getMeals = async (event) => {
+  componentDidMount = async () => {
+    let response = await axios.get(mealsURL);
 
-		this.setState({
-			mealsList: response.data,
-		});
-	};
+    this.setState({
+      mealsList: response.data,
+    });
+  };
 
-	render() {
-		return <div className="App"></div>;
-	}
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
