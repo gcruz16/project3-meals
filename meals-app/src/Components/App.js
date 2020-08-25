b59ffc934abc6f0a44779da62174be120f048a32
hx08371 <brammannjeffreyt@johndeere.com>
2020-08-25 10:06:40 -0500

    fixed the default category

--- meals-app/src/Components/App.js
+++ meals-app/src/Components/App.js
@@ -8,70 +8,72 @@ import "./App.css";
 const categoryURL = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			mealCategories: [],
-			categoryMeals: [],
-			mealDetails: [],
-		};
-	}
-	//   getMeals = async (event) => {
-	componentDidMount = async () => {
-		let response = await axios.get(categoryURL);
+  constructor() {
+    super();
+    this.state = {
+      mealCategories: [],
+      categoryMeals: [],
+      mealDetails: [],
+    };
+  }
 
-		this.setState({
-			mealCategories: response.data.categories,
-		});
-	};
+  getCategoryMeals = async (event) => {
+    let changeCategory = event.target.value;
+    const mealsURL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${changeCategory}`;
+    let response = await axios.get(mealsURL);
+    this.setState({
+      categoryMeals: response.data.meals,
+    });
+  };
 
-	getCategoryMeals = async (event) => {
-		let changeCategory = event.target.value;
-		const mealsURL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${changeCategory}`;
-		let response = await axios.get(mealsURL);
-		this.setState({
-			categoryMeals: response.data.meals,
-		});
-	};
+  componentDidMount = async () => {
+    let response = await axios.get(categoryURL);
+    const mealsURL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef`;
+    let mealResponse = await axios.get(mealsURL);
+    this.setState({
+      categoryMeals: mealResponse.data.meals,
+      mealCategories: response.data.categories,
+    });
+  };
 
-	getMealDetails = async (event) => {
-		let meal = event.target.getAttribute("alt");
-		const mealURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
-		let response = await axios.get(mealURL);
-		this.setState({
-			mealDetails: response.data.meals,
-		});
-	};
+  getMealDetails = async (event) => {
+    let meal = event.target.getAttribute("alt");
+    const mealURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
+    let response = await axios.get(mealURL);
+    this.setState({
+      mealDetails: response.data.meals,
+    });
+  };
 
-	render() {
-		return (
-			<div className="App">
-				<h1>The Meal DB</h1>
-				<main>
-					<Switch>
-						<Route
-							exact
-							path="/"
-							render={(routerProps) => (
-								<MealCategories
-									{...this.state}
-									getCategoryMeals={this.getCategoryMeals}
-									getMealDetails={this.getMealDetails}
-									{...routerProps}
-								/>
-							)}
-						/>
-						<Route
-							path="/meals/:strMeal"
-							render={(routerProps) => (
-								<MealDetails {...this.state} {...routerProps} />
-							)}
-						/>
-					</Switch>
-				</main>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <h1>The Meal DB</h1>
+        <main>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(routerProps) => (
+                <MealCategories
+                  {...this.state}
+                  getCategoryMeals={this.getCategoryMeals}
+                  getMealDetails={this.getMealDetails}
+                  {...routerProps}
+                />
+              )}
+            />
+            <Route
+              path="/meals/:strMeal"
+              render={(routerProps) => (
+                <MealDetails {...this.state} {...routerProps} />
+              )}
+            />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- meals-app/src/MealCategories.js
+++ meals-app/src/MealCategories.js
@@ -3,39 +3,39 @@ import { Link } from "react-router-dom";
 import "./MealCategories.css";
 
 function MealCategories(props) {
-	console.log(props);
+  console.log(props);
 
-	const optionCategory = props.mealCategories.map((category, index) => {
-		return <option key={index + 1}>{category.strCategory}</option>;
-	});
+  const optionCategory = props.mealCategories.map((category, index) => {
+    return <option key={index + 1}>{category.strCategory}</option>;
+  });
 
-	const mealsArray = props.categoryMeals.map((meal, index) => {
-		return (
-			<div key={index}>
-				<div id="mealDiv">
-					<Link to={`/meals/${meal.strMeal}`}>
-						<img
-							id="mealImage"
-							alt={meal.strMeal}
-							onClick={props.getMealDetails}
-							src={meal.strMealThumb}
-						/>
-					</Link>
-					<p id="mealName">{meal.strMeal}</p>
-				</div>
-			</div>
-		);
-	});
-	return (
-		<div>
-			<div>
-				<p>Meal Category</p>
-				<select onChange={props.getCategoryMeals}>{optionCategory}</select>
-			</div>
-			<div id="catMeals">
-				<div id="catMealHeading">{mealsArray}</div>
-			</div>
-		</div>
-	);
+  const mealsArray = props.categoryMeals.map((meal, index) => {
+    return (
+      <div key={index}>
+        <div id="mealDiv">
+          <Link to={`/meals/${meal.strMeal}`}>
+            <img
+              id="mealImage"
+              alt={meal.strMeal}
+              onClick={props.getMealDetails}
+              src={meal.strMealThumb}
+            />
+          </Link>
+          <p id="mealName">{meal.strMeal}</p>
+        </div>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <div>
+        <p>Select a Meal Category</p>
+        <select onChange={props.getCategoryMeals}>{optionCategory}</select>
+      </div>
+      <div id="catMeals">
+        <div id="catMealHeading">{mealsArray}</div>
+      </div>
+    </div>
+  );
 }
 export default MealCategories;
