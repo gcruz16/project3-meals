5f7489936b3935e38437686fe6782a6bdc357760
Gladys Cruz <cruzgladysazucena@johndeere.com>
2020-08-25 18:48:51 -0500

    adding some style to MealCategories,App and MealDetails components

--- meals-app/src/Components/App.js
+++ meals-app/src/Components/App.js
@@ -8,84 +8,85 @@ import "./App.css";
 const categoryURL = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      mealCategories: [],
-      categoryMeals: [],
-      mealDetails: [],
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			mealCategories: [],
+			categoryMeals: [],
+			mealDetails: [],
+		};
+	}
 
-  getCategoryMeals = async (event) => {
-    let changeCategory = event.target.value;
-    const mealsURL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${changeCategory}`;
-    let response = await axios.get(mealsURL);
-    this.setState({
-      categoryMeals: response.data.meals,
-    });
-  };
+	getCategoryMeals = async (event) => {
+		let changeCategory = event.target.value;
+		const mealsURL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${changeCategory}`;
+		let response = await axios.get(mealsURL);
+		this.setState({
+			categoryMeals: response.data.meals,
+		});
+	};
 
-  componentDidMount = async () => {
-    let response = await axios.get(categoryURL);
-    const mealsURL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef`;
-    let mealResponse = await axios.get(mealsURL);
-    this.setState({
-      categoryMeals: mealResponse.data.meals,
-      mealCategories: response.data.categories,
-    });
-  };
+	componentDidMount = async () => {
+		let response = await axios.get(categoryURL);
+		const mealsURL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef`;
+		let mealResponse = await axios.get(mealsURL);
+		this.setState({
+			categoryMeals: mealResponse.data.meals,
+			mealCategories: response.data.categories,
+		});
+	};
 
-  getMealDetails = async (event) => {
-    let meal = event.target.getAttribute("alt");
-    const mealURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
-    let response = await axios.get(mealURL);
-    this.setState({
-      mealDetails: response.data.meals,
-    });
-  };
+	getMealDetails = async (event) => {
+		let meal = event.target.getAttribute("alt");
+		const mealURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
+		let response = await axios.get(mealURL);
+		this.setState({
+			mealDetails: response.data.meals,
+		});
+	};
 
-  render() {
-    return (
-      <div className="App">
-        <div id="pageHeading">
-          <div id="titleDiv">
-            <h1 id="pageTitle">The Meal Database</h1>
-          </div>
-          <div id="imageDiv">
-            <img
-              id="pageImage"
-              src="https://media.self.com/photos/5ec2fa11589de8da3fd4ccbd/master/w_1280,h_960,c_limit/veestro two final.png"
-            />
-          </div>
-        </div>
-        <main>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={(routerProps) => (
-                <MealCategories
-                  {...this.state}
-                  getCategoryMeals={this.getCategoryMeals}
-                  getMealDetails={this.getMealDetails}
-                  {...routerProps}
-                />
-              )}
-            />
-            <div id="mealDetails">
-              <Route
-                path="/meals/:strMeal"
-                render={(routerProps) => (
-                  <MealDetails {...this.state} {...routerProps} />
-                )}
-              />
-            </div>
-          </Switch>
-        </main>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div id="pageHeading">
+					<div id="titleDiv">
+						<h1 id="pageTitle">The Meal Database</h1>
+					</div>
+					<div id="imageDiv">
+						<img
+							alt=""
+							id="pageImage"
+							src="https://media.self.com/photos/5ec2fa11589de8da3fd4ccbd/master/w_1280,h_960,c_limit/veestro two final.png"
+						/>
+					</div>
+				</div>
+				<main>
+					<Switch>
+						<Route
+							exact
+							path="/"
+							render={(routerProps) => (
+								<MealCategories
+									{...this.state}
+									getCategoryMeals={this.getCategoryMeals}
+									getMealDetails={this.getMealDetails}
+									{...routerProps}
+								/>
+							)}
+						/>
+						<div id="mealDetails">
+							<Route
+								path="/meals/:strMeal"
+								render={(routerProps) => (
+									<MealDetails {...this.state} {...routerProps} />
+								)}
+							/>
+						</div>
+					</Switch>
+				</main>
+			</div>
+		);
+	}
 }
 
 export default App;
--- meals-app/src/Ingredients.js
+++ meals-app/src/Ingredients.js
@@ -2,26 +2,31 @@ import React from "react";
 import "./Ingredients.css";
 
 function Ingredients(props) {
-  console.log(props);
-  console.log(props.measurementArray);
-  //   return <div>Ingredients</div>;
-  const showIngredientsArray = props.ingredientArray.map(
-    (displayIngredient, index) => {
-      return <div key={index}>{displayIngredient}</div>;
-    }
-  );
-  const showMeasurementArray = props.measurementArray.map(
-    (displayMeasurement, index) => {
-      return <div key={index}>{displayMeasurement}</div>;
-    }
-  );
-  console.log(props);
-  return (
-    <div id="ingredientData">
-      <div id="showIngredients">{showIngredientsArray}</div>
-      <div id="showMeasurements">{showMeasurementArray}</div>
-    </div>
-  );
+	const showIngredientsArray = props.ingredientArray.map(
+		(displayIngredient, index) => {
+			return (
+				<div key={index} id="ingredientItem">
+					{displayIngredient}
+				</div>
+			);
+		}
+	);
+	const showMeasurementArray = props.measurementArray.map(
+		(displayMeasurement, index) => {
+			return (
+				<div key={index} id="measurementItem">
+					{displayMeasurement}
+				</div>
+			);
+		}
+	);
+	console.log(props);
+	return (
+		<div id="ingredientData">
+			<div id="showMeasurements">{showMeasurementArray}</div>
+			<div id="showIngredients">{showIngredientsArray}</div>
+		</div>
+	);
 }
 
 export default Ingredients;
