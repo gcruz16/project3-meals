c63464de1678a1d49b078180dfec7c80dffe68d2
hx08371 <brammannjeffreyt@johndeere.com>
2020-08-26 14:55:18 -0500

    changes to css for headings and other small fixes

--- meals-app/src/Components/App.js
+++ meals-app/src/Components/App.js
@@ -8,98 +8,99 @@ import "./App.css";
 const categoryURL = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			mealCategories: [],
-			categoryMeals: [],
-			mealDetails: [],
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      mealCategories: [],
+      categoryMeals: [],
+      mealDetails: [],
+    };
+  }
 
-	getCategoryMeals = async (event) => {
-		let changeCategory = event.target.value;
-		const mealsURL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${changeCategory}`;
-		let response = await axios.get(mealsURL);
-		this.setState({
-			categoryMeals: response.data.meals,
-		});
-	};
+  getCategoryMeals = async (event) => {
+    let changeCategory = event.target.value;
+    const mealsURL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${changeCategory}`;
+    let response = await axios.get(mealsURL);
+    this.setState({
+      categoryMeals: response.data.meals,
+    });
+  };
 
-	componentDidMount = async () => {
-		let response = await axios.get(categoryURL);
-		const mealsURL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef`;
-		let mealResponse = await axios.get(mealsURL);
-		this.setState({
-			categoryMeals: mealResponse.data.meals,
-			mealCategories: response.data.categories,
-		});
-	};
+  componentDidMount = async () => {
+    let response = await axios.get(categoryURL);
+    const mealsURL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef`;
+    let mealResponse = await axios.get(mealsURL);
+    this.setState({
+      categoryMeals: mealResponse.data.meals,
+      mealCategories: response.data.categories,
+    });
+  };
 
-	getMealDetails = async (event) => {
-		let meal = event.target.getAttribute("alt");
-		const mealURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
-		let response = await axios.get(mealURL);
-		this.setState({
-			mealDetails: response.data.meals,
-		});
-	};
+  getMealDetails = async (event) => {
+    let meal = event.target.getAttribute("alt");
+    const mealURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${meal}`;
+    let response = await axios.get(mealURL);
+    this.setState({
+      mealDetails: response.data.meals,
+    });
+  };
 
-	render() {
-		return (
-			<div className="App">
-				<div id="pageHeading">
-					<img
-						alt=""
-						src="https://image.flaticon.com/icons/svg/608/608857.svg"
-					/>
-					<div id="titleDiv">
-						<h1 id="pageTitle">The Meal Database</h1>
-					</div>
-					<div id="imageDiv">
-						<img
-							alt=""
-							id="pageImage"
-							src="https://media.self.com/photos/5ec2fa11589de8da3fd4ccbd/master/w_1280,h_960,c_limit/veestro two final.png"
-						/>
-					</div>
-					<div id="menuIcons">
-						<Link to="/" id="homePage">
-							<img
-								alt=""
-								id="iconHomePage"
-								src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSSmTFP1d8qpA1pcUhE1RWfz-o_JLSYGqSKIw&usqp=CAU"
-							/>
-						</Link>
-					</div>
-				</div>
-				<main>
-					<Switch>
-						<Route
-							exact
-							path="/"
-							render={(routerProps) => (
-								<MealCategories
-									{...this.state}
-									getCategoryMeals={this.getCategoryMeals}
-									getMealDetails={this.getMealDetails}
-									{...routerProps}
-								/>
-							)}
-						/>
-						<div id="mealDetails">
-							<Route
-								path="/meals/:strMeal"
-								render={(routerProps) => (
-									<MealDetails {...this.state} {...routerProps} />
-								)}
-							/>
-						</div>
-					</Switch>
-				</main>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <div id="pageHeading">
+          <img
+            id="plateImg"
+            alt=""
+            src="https://image.flaticon.com/icons/svg/608/608857.svg"
+          />
+          <div id="titleDiv">
+            <h1 id="pageTitle">The Meal Database</h1>
+          </div>
+          <div id="imageDiv">
+            <img
+              alt=""
+              id="pageImage"
+              src="https://media.self.com/photos/5ec2fa11589de8da3fd4ccbd/master/w_1280,h_960,c_limit/veestro two final.png"
+            />
+          </div>
+          <div id="menuIcons">
+            <Link to="/" id="homePage">
+              <img
+                alt=""
+                id="iconHomePage"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSSmTFP1d8qpA1pcUhE1RWfz-o_JLSYGqSKIw&usqp=CAU"
+              />
+            </Link>
+          </div>
+        </div>
+        <main>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(routerProps) => (
+                <MealCategories
+                  {...this.state}
+                  getCategoryMeals={this.getCategoryMeals}
+                  getMealDetails={this.getMealDetails}
+                  {...routerProps}
+                />
+              )}
+            />
+            <div id="mealDetails">
+              <Route
+                path="/meals/:strMeal"
+                render={(routerProps) => (
+                  <MealDetails {...this.state} {...routerProps} />
+                )}
+              />
+            </div>
+          </Switch>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
